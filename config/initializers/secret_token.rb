e8ce2701d79e6af5008b40cdc4c57623ec5640e8
Ruthie Nachmany <ruthienachmany@gmail.com>
2013-08-19 16:57:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterReader::Application.config.secret_token = 'a618980cf26cebb5a27165eca002b5c7bf1fd3caf6fcb4e1c718c9f5e2a3b5f7a8c9281abc5d11fc8e7160f26a2066c551f1f0837e8d691c327e9d086509a1cb'
