6cf55e52f71b409ec07a84931e04d2a0b29f32f1
Ruthie Nachmany <ruthienachmany@gmail.com>
2013-09-06 16:18:43 -0400

    added flat ui assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,28 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
+
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+
+//= require jquery
+//= require jquery_ujs
+//= require ../../../vendor/assets/javascripts/bootstrap-tooltip.js
+//= require ../../../vendor/assets/javascripts/jquery-1.8.2.min.js
+//= require ../../../vendor/assets/javascripts/jquery-ui-1.10.0.custom.min.js
+//= require ../../../vendor/assets/javascripts/jquery.dropkick-1.0.0.js
+//= require ../../../vendor/assets/javascripts/custom_checkbox_and_radio.js
+//= require ../../../vendor/assets/javascripts/custom_radio.js
+//= require ../../../vendor/assets/javascripts/jquery.tagsinput.js
+//= require ../../../vendor/assets/javascripts/bootstrap-tooltip.js
+//= require ../../../vendor/assets/javascripts/jquery.placeholder.js
+//= require ../../../vendor/assets/javascripts/application.js
